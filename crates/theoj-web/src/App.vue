3bc872f99af26ca033fbfe00485f82f72ec88531
paran3xus <paran3xus007@gmail.com>
2025-10-14 18:18:19 +0800

    feat: set token

--- crates/theoj-web/src/App.vue
+++ crates/theoj-web/src/App.vue
@@ -2,18 +2,26 @@
 import { OpenAPI } from "@/theoj-api";
 import Footer from "./components/Footer.vue";
 import Header from "./components/Header.vue";
+import { useUserStore } from "./user.mts";
 
 if (import.meta.env.DEV) {
   OpenAPI.BASE = "http://localhost:8080";
 } else {
   OpenAPI.BASE = window.API_ROOT || "";
 }
+
+const userStore = useUserStore();
+
+if (userStore.isLoggedIn) {
+  OpenAPI.TOKEN = userStore.token;
+}
+
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col bg-base-200">
     <Header />
-    <div class="flex-1 mx-auto w-full max-w-6xl px-8 lg:px-16 flex flex-col">
+    <div class="flex-1 mx-auto w-full max-w-6xl px-8 py-8 lg:px-16 flex flex-col">
       <router-view />
     </div>
     <Footer />
