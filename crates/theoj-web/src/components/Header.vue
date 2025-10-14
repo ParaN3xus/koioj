aadb8200ea0855cb1b8370dbb2da3b9d92b5744f
paran3xus <paran3xus007@gmail.com>
2025-10-14 18:17:25 +0800

    feat: add tailwind css `mlg` breakpoint
    mlg

--- crates/theoj-web/src/components/Header.vue
+++ crates/theoj-web/src/components/Header.vue
@@ -11,12 +11,13 @@ const navButtons = [
 const siteTitle = "TheOJ";
 
 const userStore = useUserStore();
+
 </script>
 
 <template>
   <header class="bg-base-100 sticky top-0 z-[1] bg-opacity-80 backdrop-blur shadow-lg">
-    <div class="navbar mx-auto max-w-6xl md:px-16">
-      <div class="navbar-start md:hidden">
+    <div class="navbar mx-auto max-w-6xl mlg:px-16">
+      <div class="navbar-start mlg:hidden">
         <div class="dropdown">
           <div tabindex="0" class="btn btn-ghost btn-circle">
             <Icon icon="fa7-solid:bars" width="20" />
@@ -31,9 +32,9 @@ const userStore = useUserStore();
         </div>
       </div>
 
-      <div class="navbar-start hidden md:flex md:flex-1">
+      <div class="navbar-start hidden mlg:flex mlg:flex-1">
         <RouterLink class="btn btn-ghost text-xl font-bold mr-8" to="/">{{ siteTitle }}</RouterLink>
-        <div class="hidden md:flex md:space-x-2">
+        <div class="hidden mlg:flex mlg:space-x-2">
           <RouterLink v-for="item in navButtons" :key="item.to" class="btn btn-ghost w-18 text-base" :to="item.to">
             <Icon :icon="item.icon" width="20" class="mr-1" />
             {{ item.text }}
@@ -41,21 +42,22 @@ const userStore = useUserStore();
         </div>
       </div>
 
-      <div class="navbar-center md:hidden">
+      <div class="navbar-center mlg:hidden">
         <RouterLink class="btn btn-ghost text-xl font-bold" to="/">{{ siteTitle }}</RouterLink>
       </div>
 
-      <div class="navbar-end space-x-2 md:flex-none md:w-auto">
+      <div class="navbar-end space-x-2 mlg:flex-none mlg:w-auto">
         <template v-if="userStore.isLoggedIn">
-          <RouterLink class="flex items-center btn btn-ghost btn-circle justify-center rounded-full" to="/profile">
+          <RouterLink class="flex items-center btn btn-ghost btn-circle justify-center rounded-full"
+            :to="`/users/profile/${userStore.userId}`">
             <div class="w-10 h-10 flex items-center justify-center rounded-full bg-base-300 ">
               <Icon icon="fa6-solid:user" width="16" />
             </div>
           </RouterLink>
         </template>
         <template v-else>
-          <RouterLink class="btn btn-ghost btn-sm" to="/user/login">Login</RouterLink>
-          <RouterLink class="btn btn-primary btn-sm" to="/user/register">Register</RouterLink>
+          <RouterLink class="btn btn-ghost btn-sm" to="/users/login">Login</RouterLink>
+          <RouterLink class="btn btn-primary btn-sm" to="/users/register">Register</RouterLink>
         </template>
         <ThemeToggle />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'mlg': '896px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [
     daisyui,
