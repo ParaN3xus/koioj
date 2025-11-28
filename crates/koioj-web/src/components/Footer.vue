a497bfb58dcdc99175ecd988e7ffc12423c3fe44
paran3xus <paran3xus007@gmail.com>
2025-11-28 12:36:57 +0800

    refactor(web): footer style

--- crates/koioj-web/src/components/Footer.vue
+++ crates/koioj-web/src/components/Footer.vue
@@ -5,7 +5,8 @@ const currentYear = new Date().getFullYear();
 </script>
 
 <template>
-  <footer class="bg-base-100 border-t border-base-300 mt-auto">
+  <footer
+    class="bg-base-100 bg-opacity-80 mt-auto shadow-[0_-10px_15px_-3px_rgba(0,0,0,0.1),0_-4px_6px_-4px_rgba(0,0,0,0.1)]">
     <div class="mx-auto max-w-6xl px-4 py-4 md:px-8 md:py-6">
       <div class="flex flex-col md:flex-row justify-between items-center gap-3 md:gap-4">
         <div class="flex flex-col md:flex-row items-center gap-1 md:gap-4 text-center md:text-left">
